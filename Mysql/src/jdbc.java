b75572305575e10b3ca70818a97b90dcd4f4f2ae
Yingxie Gao <yingxieg@iastate.edu>
2019-05-31 23:37:41 +0800

    查询和修改

--- Mysql/src/jdbc.java
+++ Mysql/src/jdbc.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class jdbc {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -8,5 +6,18 @@ public class jdbc {
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb1", "root", "156292170");
         System.out.println(con);
 
+        Statement stmt = con.createStatement();
+        
+        /*String sql = "insert into stu values(7,'niubi')";
+        int r = stmt.executeUpdate(sql);
+        System.out.println(r);
+*/
+        ResultSet result= stmt.executeQuery("select * from stu");
+        while(result.next())
+        {
+            int id = result.getInt(1);
+            String name = result.getString("name");
+            System.out.println(id + ", "+ name);
+        }
     }
 }
